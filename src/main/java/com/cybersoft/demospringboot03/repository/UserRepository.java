8b5d6312d1de5026e4b800c4b04ab5ffe862a7ae
happyday <jsoibn@gmail.com>
2023-11-17 20:16:32 +0700

    feature: jpa/IOCConfig

--- src/main/java/com/cybersoft/demospringboot03/repository/UserRepository.java
+++ src/main/java/com/cybersoft/demospringboot03/repository/UserRepository.java
@@ -4,7 +4,9 @@ import com.cybersoft.demospringboot03.entity.UsersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<UsersEntity, Integer> {
-
+    List<UsersEntity> findByEmailAndPassword(String email, String password);
 }
